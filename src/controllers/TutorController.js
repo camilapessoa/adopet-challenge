b19ae189abe32bcd8d0c79eb91e3a62a101b1e58
lmoraesdev <lbc_moraes@outlook.com>
2023-04-24 15:37:47 -0300

    feat:autenticação de usuario

--- src/controllers/TutorController.js
+++ src/controllers/TutorController.js
@@ -3,23 +3,20 @@ const tutorServices = new Services("Tutor");
 const autenticarUsuario = require('../utils/autenticarUsuario');
 
 class TutorController {
-  
+
   static async autenticar(req, res) {
     const { email, senha } = req.body;
-    try {
-      const tutor = await tutorServices.pegaUmRegistro({ email: email });
+
+    const tutor = await tutorServices.pegaUmRegistro({ email: email });
       if (!tutor) {
         return res.status(401).json({ message: 'Usuário não encontrado' });
-      }
-      const autenticado = autenticarUsuario(senha, tutor);
+     }
+     const autenticado = autenticarUsuario(senha, tutor);
       if (autenticado) {
         return res.status(200).json({ message: 'Usuário autenticado com sucesso' });
       } else {
         return res.status(401).json({ message: 'Senha incorreta' });
-      }
-    } catch (error) {
-      return res.status(500).json(error.message);
-    }
+     }
   }
 
   static async getAllTutors(req, res) {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const routes = require("./routes");
+const exceptionHandler = require('./middlewares/exceptionHandler')
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 routes(app);
+
+app.use(exceptionHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening at http:localhost:${port}`);
